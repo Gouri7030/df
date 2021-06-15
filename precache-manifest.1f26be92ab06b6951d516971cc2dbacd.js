253dfd893db9942d43479268d62c3c8c51d96c46
Gouri <sutar0205@gmail.com>
2021-06-15 14:55:02 +0530

    Updates

---
+++ precache-manifest.1f26be92ab06b6951d516971cc2dbacd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33339b7b0a2b6a12a56a",
+    "url": "/df/static/css/main.61e8a2a4.chunk.css"
+  },
+  {
+    "revision": "33339b7b0a2b6a12a56a",
+    "url": "/df/static/js/main.33339b7b.chunk.js"
+  },
+  {
+    "revision": "d2a72ad7e0b312479945",
+    "url": "/df/static/js/1.d2a72ad7.chunk.js"
+  },
+  {
+    "revision": "c5b0740dec5f329f3549",
+    "url": "/df/static/js/runtime~main.c5b0740d.js"
+  },
+  {
+    "revision": "67d73bc22dfc5038e6ac945e6953a4bf",
+    "url": "/df/index.html"
+  }
+];
